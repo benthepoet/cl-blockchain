81f1c872534db19d72936e1d9ee922721dad0790
Ben Hanna <benpaulhanna@gmail.com>
2019-06-24 13:32:21 -0400

    Implemented the chain data structure.

--- package.lisp
+++ package.lisp
@@ -9,7 +9,9 @@
    #:cl
    #:ironclad)
   (:export
-   #:make-block))
+   #:add-block
+   #:make-block
+   #:make-chain))
 
 (in-package #:cl-blockchain)
 
@@ -19,15 +21,28 @@
     :accessor timestamp)
    (hash
     :initarg :hash
-    :accessor hash)))
+    :accessor hash)
+   (previous-hash
+    :initarg :previous-hash
+    :accessor previous-hash)))
 
-(defun hash-block (timestamp)
-  (let ((data (ironclad:integer-to-octets timestamp)))
+(defun add-block (chain)
+  (let* ((last-block (car chain))
+         (new-block (make-block (hash last-block))))
+    (append (list new-block) chain)))
+
+(defun hash-block (timestamp previous-hash)
+  (let ((data (ironclad:ascii-string-to-byte-array
+               (format nil "~d~A" timestamp previous-hash))))
     (ironclad:byte-array-to-hex-string
      (ironclad:digest-sequence :sha256 data))))
 
-(defun make-block ()
+(defun make-block (previous-hash)
   (let* ((timestamp (get-universal-time)))
     (make-instance 'block-object
                    :timestamp timestamp
-                   :hash (hash-block timestamp))))
+                   :hash (hash-block timestamp previous-hash)
+                   :previous-hash previous-hash)))
+
+(defun make-chain ()
+  (list (make-block nil)))
